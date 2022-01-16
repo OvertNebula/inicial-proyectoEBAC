4bc868349de66a7736ad6a0ed47d65d6cc19d855
OvertNebula <96168378+OvertNebula@users.noreply.github.com>
2022-01-16 13:12:43 -0600

    experimentando un poco
    - no se porque pero perdi lo que estaba haciendo aunque no fue mucho

--- Assets/scrips/HolaMundo.cs
+++ Assets/scrips/HolaMundo.cs
@@ -1,14 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+
+
+
+    int x;
     // Start is called before the first frame update
     void Start()
     {
+        x = 0;
         print("aglo paso");
-        Debug.Log("algo paso");
+
         Debug.LogWarning("algo salio medianamente mal");
         Debug.LogError("algo salio muy mal");
     }
@@ -16,6 +19,7 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        x = x + 1;
+        Debug.Log(x);
     }
 }

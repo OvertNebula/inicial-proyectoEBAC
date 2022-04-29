b7ba2da44dccffe81a0be9c7d236ae2825cef36e
OvertNebula <96168378+OvertNebula@users.noreply.github.com>
2022-04-29 12:52:56 -0500

    prectica7
    -agregado del flase y true

--- Assets/scrips/VariablesNuemericas.cs
+++ Assets/scrips/VariablesNuemericas.cs
@@ -12,11 +12,23 @@ public class VariablesNuemericas : MonoBehaviour
     //float [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0]
     //double[0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0]
 
+    sbyte miByteConsigno = 0;
+    byte miByte = 0;
+    short mishortSinSigno = 0;
+    int miTnt = 0;
+    uint miIntSinSigno = 0;
+    long miLong = 0;
+    ulong miLongSiSingno = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        miByteConsigno = 25;
+        miTnt = miByteConsigno;
+        Debug.Log("el valor de miByteConSigno" + miByteConsigno);
+        Debug.Log("el valor de miInt" + miTnt);
+        miByte = 200;
+        miIntSinSigno = miByte;
     }
 
     // Update is called once per frame

1d211a9dcfaa46bbe84c3bac4fb870fb98e5a16f
OvertNebula <96168378+OvertNebula@users.noreply.github.com>
2022-05-18 18:14:59 -0500

    pratica8
    primer push de la practica 8 con solo las variables numéricas y un error

--- Assets/scrips/VariablesNuemericas.cs
+++ Assets/scrips/VariablesNuemericas.cs
@@ -14,8 +14,8 @@ public class VariablesNuemericas : MonoBehaviour
 
     sbyte miByteConsigno = 0;
     byte miByte = 0;
-    short mishortSinSigno = 0;
-    int miTnt = 0;
+    short mishort = 0;
+    int miInt = 0;
     uint miIntSinSigno = 0;
     long miLong = 0;
     ulong miLongSiSingno = 0;
@@ -24,16 +24,55 @@ public class VariablesNuemericas : MonoBehaviour
     void Start()
     {
         miByteConsigno = 25;
-        miTnt = miByteConsigno;
+        miInt = miByteConsigno;
         Debug.Log("el valor de miByteConSigno" + miByteConsigno);
-        Debug.Log("el valor de miInt" + miTnt);
+        Debug.Log("el valor de miInt" + miInt);
         miByte = 200;
         miIntSinSigno = miByte;
+        miIntSinSigno = (uint)miByteConsigno;
+        Debug.Log("el valor de miInt es" + miIntSinSigno);
+
+        miLong = long.MaxValue;
+        miInt = (int)miLong;
+        Debug.Log("el valor de miInt es:" + miInt);
+
+        int a, b, c;
+        a = 5;
+        b = 10;
+        c = a + b;
+        Debug.Log(c);
+        c = a * b;
+        Debug.Log(c);
+        c = a - b;
+        Debug.Log(c);
+        c = a / b;
+        Debug.Log(c);
+        float f;
+        f = (a / b);
+        Debug.Log(f);
+
+        miInt = Random.Range (a, b);
+        if (miInt % 2 == 0)
+        {
+            Debug.Log("el numero" + miInt + "es par");
+        }
+        else
+        {
+            Debug.Log("el numero" + miInt + "es non");
+        }
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mishort *= 3;
+        Debug.Log(mishort);
+
+        miLong += 2;         // esta y
+        miLong = miLong + 2; // esta son lo mismo
+        Debug.Log(miLong);
     }
 }

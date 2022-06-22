61e32b9475c8238f00098c448f73dd627fb09f99
OvertNebula <96168378+OvertNebula@users.noreply.github.com>
2022-06-22 02:32:50 -0500

    practica8
    -agregando la (palabra3)

--- Assets/scrips/StringOperaciones.cs
+++ Assets/scrips/StringOperaciones.cs
@@ -15,17 +15,20 @@ public class StringOperaciones : MonoBehaviour
     {
         string palabra1 = "elefante";
         string cambio1 = palabra1.Replace("f", "g");
-        Debug.Log(cambio1);
+        Debug.Log(cambio1); 
 
         //char[] cambio1 = palabra1.ToCharArray();
         //cambio1[4] = 'g';
         //Debug.Log(cambio1);
 
         string palabra2 = "no creo";
-        string cambio2 = palabra2.Replace("no","si");
+        string cambio2 = palabra2.Replace("no", "si");
         Debug.Log(cambio2);
 
-        string palabra3;
+        string palabra3 = "me trade";
+        string[] cambio3 = palabra3.Split('%');
+        Debug.Log(cambio3);
+        
 
         string palabra4 = "tengo mucho frio";
         string cambio4 = palabra4.Remove(6,5);

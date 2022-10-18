cc547457d9e59d72e9c2862516683ce7be1629bd
OvertNebula <96168378+OvertNebula@users.noreply.github.com>
2022-10-18 06:33:28 -0500

    practica12
    versión-3

--- Assets/scrips/EjerciciosEstructuras.cs
+++ Assets/scrips/EjerciciosEstructuras.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class EjerciciosEstructuras : MonoBehaviour
 {
+    public List<int> p12MiFuncion3 = new List<int>();
     public List<int> p12MiFuncion2 = new List<int>();
     //public List<int> p12MiFuncion(int tamaño, int rangoInferior, int rangoSuperior);
     // Start is called before the first frame update
     public void Start()
     {
         //p12MiFuncion1(10,0,20);
-        //list<int> RegresoDep12MiFuncion1 = p12MiFuncion1;
+        //ist<int> RegresoDep12MiFuncion1 = p12MiFuncion1;
         //Debug.Log("MiFuncion1 " + RegresoDep12MiFuncion1);
 
         p12MiFuncion2.Sort();
@@ -25,10 +26,20 @@ public class EjerciciosEstructuras : MonoBehaviour
         {
             Debug.Log("MiFuncion2 " + Fun2);
         }
+
+        p12MiFuncion3.Add(1);
+        p12MiFuncion3.Add(1);
+        p12MiFuncion3.Add(2);
+        p12MiFuncion3.Add(2);
+        p12MiFuncion3.Add(3);
+        p12MiFuncion3.Add(3);
+        p12MiFuncion3.Add(4);
+        p12MiFuncion3.Add(4);
+        
     }
 
     // Update is called once per frame
-    //public list<int> p12MiFuncion1 (int tamaño, int rangoInferior, int rangoSuperior)
+    //public List<int> p12MiFuncion1 (int tamaño, int rangoInferior, int rangoSuperior)
     //{
     //    tamaño = (Random.Range(rangoInferior, rangoSuperior));
     //    Debug.Log("MiFuncion1 " + tamaño);
@@ -42,6 +53,14 @@ public class EjerciciosEstructuras : MonoBehaviour
             Debug.Log("MiFuncion2 " + Fun2);
         }
 
+    }
+
+    public void MiFuncion3()
+    {
+        foreach (int Fun3 in p12MiFuncion3)
+        {
+
+        }
     }
 
 

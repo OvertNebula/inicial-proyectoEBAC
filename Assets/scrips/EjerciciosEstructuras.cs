1b31c691732eb751b12e03c16dd5e1eb3e507c4c
OvertNebula <96168378+OvertNebula@users.noreply.github.com>
2022-11-15 05:04:31 -0600

    practica11
    correcciones del tutor

--- Assets/scrips/EjerciciosEstructuras.cs
+++ Assets/scrips/EjerciciosEstructuras.cs
@@ -12,9 +12,9 @@ public class EjerciciosEstructuras : MonoBehaviour
     public void Start()
     {
 
-        //List<int> RegresoDep12MiFuncion1 = p11MiFuncion1(10, 0, 20);
-        ////p12MiFuncion1(10, 0, 20);
-        //Debug.Log("MiFuncion1 " + RegresoDep12MiFuncion1);
+        List<int> RegresoDep12MiFuncion1 = p11MiFuncion1(10, 0, 20);
+        //p12MiFuncion1(10, 0, 20);
+        Debug.Log("MiFuncion1 " + RegresoDep12MiFuncion1);
 
         p11MiFuncion2.Add(9);
         p11MiFuncion2.Add(0);
@@ -40,6 +40,11 @@ public class EjerciciosEstructuras : MonoBehaviour
         p11MiFuncion3.Add(4);
         p11MiFuncion3.Add(4);
 
+        foreach (int Fun3 in p11MiFuncion3)
+        {
+            Debug.Log("MiFuncion3 " + Fun3);
+        }
+
 
         p11MiFuncion4.Push("A");
         p11MiFuncion4.Push("B");
@@ -52,14 +57,17 @@ public class EjerciciosEstructuras : MonoBehaviour
     //Prueba
 
     // Update is called once per frame
-    //public List<int> p11MiFuncion1 (int tamaño, int rangoInferior, int rangoSuperior)
-    //{
-    //    tamaño = (Random.Range(rangoInferior, rangoSuperior));
-    //    List<int> tamañoDebuelto = p11MiFuncion1;
-    //    Debug.Log("MiFuncion1 " + tamaño);
-    //
-    //    return tamañoDebuelto;
-    //}
+    public List<int> p11MiFuncion1 (int tamaño, int rangoInferior, int rangoSuperior)
+    {
+        List<int> tamañoDebuelto = new List<int>();
+
+        for (int i =0; i< tamaño; i++)
+        {
+            tamañoDebuelto.Add(Random.Range(rangoInferior, rangoSuperior));
+        }
+    
+        return tamañoDebuelto;
+    }
     public void MiFuncion2()
     {
         foreach (int Fun2 in p11MiFuncion2)
@@ -69,32 +77,35 @@ public class EjerciciosEstructuras : MonoBehaviour
 
     }
 
-    //public HashSet<int> MiFuncion3()
-    //{
-    //    HashSet<int>MiFuncion3retorno = p11MiFuncion3
-    //    return MiFuncion3retorno;
-    //}
+    public HashSet<int> MiFuncion3(List<string> Repetidos)
+    {
+        HashSet<int> NoRepetidos = new HashSet<int>();
+        foreach (string elementos in Repetidos)
+        {
+            NoRepetidos.Add(int.Parse(elementos));
+        }
+        return NoRepetidos;
+    }
 
 
 
     public void MiFuncion4()
     {
-        foreach (string Fun4 in p11MiFuncion4)
+        Queue<string> textosDelQueue = new Queue<string> ();
+
+        int Stack = p11MiFuncion4.Count;
+        for (int i = 0; i < Stack; i++)
         {
-            Debug.Log(p11MiFuncion4.Peek());
+            Debug.Log ("MiFuncion4 Stack: " + p11MiFuncion4.Peek());
+            textosDelQueue.Enqueue(p11MiFuncion4.Peek());
             p11MiFuncion4.Pop();
-            Debug.Log(p11MiFuncion4.Peek());
-            p11MiFuncion4.Pop();
-            Debug.Log(p11MiFuncion4.Peek());
-            p11MiFuncion4.Pop();
-            Debug.Log(p11MiFuncion4.Peek());
-            p11MiFuncion4.Pop();
-            Debug.Log(p11MiFuncion4.Peek());
-            p11MiFuncion4.Pop();
-            Debug.Log(p11MiFuncion4.Peek());
-            p11MiFuncion4.Pop();
-            Debug.Log(p11MiFuncion4.Peek());
-            p11MiFuncion4.Pop();
+        }
+
+        int Queue = textosDelQueue.Count;
+        for (int i =0;i<Queue; i++)
+        {
+            Debug.Log("MiFuncion4 Queue: " + textosDelQueue.Peek());
+            textosDelQueue.Dequeue();
         }
     }
 
